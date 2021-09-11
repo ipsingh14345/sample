b7bb32c04a699e81f4914102dbd4c58e55ddd0ae
ipsingh14345 <singhikbalpreet512@gmail.com>
2021-09-11 12:19:36 +0530

    ffggfgg

--- sketch.js
+++ sketch.js
@@ -40,8 +40,7 @@ function draw(){
     background(bg);
     Engine.update(engine);
     console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+  
     box1.display();
     box2.display();
     ground.display();
